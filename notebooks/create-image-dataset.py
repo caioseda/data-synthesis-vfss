a551443f0228ca2b487f56034273df4b2670e737
Caio Seda <caioseda@hotmail.com>
2025-05-19 21:54:20 -0300

    feat: add command line argument parsing for video and labels paths

--- notebooks/create-image-dataset.py
+++ notebooks/create-image-dataset.py
@@ -9,13 +9,29 @@ from src.video_labels import read_video_labels_df
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Process some integers.")
+
+    parser.add_argument('--video', type=str, default='data/videos/', help='Path to the video directory')
+    parser.add_argument('--labels', type=str, default='data/rotulos/Frames e PAS.xlsx', help='Path to the labels file')
+    parser.add_argument('--video_id', type=str, default='1', help='ID of the video to play')
+    return parser.parse_args()
+
 if __name__ == "__main__":
-    # Define the path to the Excel file
-    path = 'data/rotulos/Frames e PAS.xlsx'
-    
+    # Parse command line arguments
+    args = parse_args()
+    # Get the video path from the command line arguments
     # Read the rotulos DataFrame
-    df_frames_pas = read_video_labels_df(path)
-    print(df_frames_pas.head())
-    play_video('data/videos/1.avi')
+    df_frames_pas = read_video_labels_df(args.labels)
+    
+    print(df_frames_pas)
+    play_video(
+        1,
+        video_dir=args.video,
+        start_frame=0,
+        end_frame=None
+    )
     
     
