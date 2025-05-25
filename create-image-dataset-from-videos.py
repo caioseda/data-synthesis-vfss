d61023957be2444540e83fe02c1904de857dfd95
Caio Seda <caioseda@hotmail.com>
2025-05-25 15:13:18 -0300

    Refactor dataset creation scripts and add functionality for all frames dataset
    - Updated `create_max_constriction_dataset` function to use new image and video tools.
    - Renamed `output_dir` parameter to `dataset_dir` for clarity.
    - Added `create_all_frames_dataset` function to extract and save all frames from videos.
    - Renamed script to `create-image-dataset-from-videos.py` for command-line dataset creation.
    - Implemented argument parsing and validation for video directory, labels, output directory, and dataset type.
    - Enhanced logging for better tracking of dataset creation process.

---
+++ create-image-dataset-from-videos.py
@@ -0,0 +1,107 @@
+import logging
+import os
+import argparse
+from src.video_labels import read_video_labels_df 
+from src.create_dataset import create_max_constriction_dataset, create_all_frames_dataset
+import re
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def create_dataset_dir(args):
+    """
+    Create a directory for the dataset based on the current run ID and dataset type.
+    """
+    dataset_type = args.dataset_type
+    dataset_type = dataset_type.replace('_', '-')
+
+    prev_run_dirs = []
+    if os.path.isdir(args.output_dir):
+        prev_run_dirs = [
+            d 
+            for d in os.listdir(args.output_dir) 
+            if os.path.isdir(os.path.join(args.output_dir, d))
+            and dataset_type in d
+        ]
+        print(f"Previous run dirs ({args.output_dir}): {prev_run_dirs}")
+        prev_run_ids = [re.match(r'^\d+', d) for d in prev_run_dirs]
+        print(f"Previous run IDs: {prev_run_ids}")
+        prev_run_ids = [int(d.group(0)) for d in prev_run_ids if d is not None]
+        print(f"Previous run IDs: {prev_run_ids}")
+        cur_run_id = max(prev_run_ids, default=-1) + 1
+
+    dataset_dir = f'{cur_run_id:05d}-{dataset_type}-{args.frame_size[0]}'
+    dataset_dir = os.path.join(args.output_dir, dataset_dir)
+
+    if not os.path.exists(dataset_dir):
+        os.makedirs(dataset_dir)
+        logging.info(f"Created directory: {dataset_dir}")
+    else:
+        logging.warning(f"Directory {dataset_dir} already exists. Please choose a different name or remove the existing directory.")
+    
+    return dataset_dir
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Process some integers.")
+
+    parser.add_argument('--video-dir', type=str, default='data/videos/', help='Path to the video directory')
+    parser.add_argument('--labels', type=str, default='data/rotulos/Frames e PAS.xlsx', help='Path to the labels file')
+    parser.add_argument('--video-id', type=str, default='1', help='ID of the video to play')
+    parser.add_argument('--output-dir', type=str, default='data/images/', help='Path to the output directory for images')
+    parser.add_argument('--frame-size', type=tuple, default=(512, 512), help='Size of the output images (width, height)')
+    parser.add_argument('--dataset-type', type=str, default='all_frames', choices=['max_constriction', 'all_frames'], help='Type of dataset to create: max_constriction or all_frames')
+    parser.add_argument('--dry-run', action='store_true', help='If set, only create the dataset directory without processing videos')
+    
+    args = parser.parse_args()
+
+    # Validating the arguments
+    if args.dataset_type not in ['max_constriction', 'all_frames']:
+        parser.error("Invalid dataset type. Choose 'max_constriction' or 'all_frames'.")
+
+    if not os.path.isdir(args.output_dir):
+        os.makedirs(args.output_dir)
+        logging.info(f"Created output directory: {args.output_dir}")
+    
+    if not os.path.isdir(args.video_dir):
+        parser.error(f"Video directory {args.video_dir} does not exist.")
+
+    if not os.path.isfile(args.labels):
+        parser.error(f"Labels file {args.labels} does not exist.")
+        
+    args.dataset_dir = create_dataset_dir(args)
+
+    return args
+
+if __name__ == "__main__":
+    # Parse command line arguments
+    args = parse_args()
+
+    if args.dry_run:
+        logging.info("Dry run mode. Only creating the dataset directory.")
+        exit(0)
+    
+    # Read the rotulos DataFrame
+    df_frames_pas = read_video_labels_df(args.labels)
+    logging.info("Labels dataframe loaded successfully.")
+
+    # Create a dataset of images from the maximum constriction frames of videos
+    if args.dataset_type == 'all_frames':
+        create_all_frames_dataset(
+            videos_dir = args.video_dir,
+            dataset_dir = args.dataset_dir,
+            frame_size = args.frame_size
+        )
+        logging.info("All frames dataset created successfully.")
+    else:
+        # Create a dataset of images from the maximum constriction frames of videos
+        create_max_constriction_dataset(
+            df_labels = df_frames_pas,
+            video_dir = args.video_dir,
+            dataset_dir = args.dataset_dir,
+            frame_size = args.frame_size
+        )
+    logging.info("Max constriction dataset created successfully.")
+    
+    
--- src/create_dataset.py
+++ src/create_dataset.py
@@ -1,5 +1,6 @@
 import os
-from .media_player import get_video_frame, save_frame_as_image
+from .video_tool import get_video_frame
+from .image_tool import save_image
 from .utils import get_video_path_from_id
 import cv2 as cv
 import logging
@@ -7,20 +8,20 @@ import logging
 # Logging configuration
 logging.getLogger()
 
-def create_max_constriction_dataset(df_labels, video_dir, output_dir, frame_size=(512, 512)):
+def create_max_constriction_dataset(df_labels, video_dir, dataset_dir, frame_size=(512, 512)):
     """
     Create a dataset of images from the maximum constriction frames of videos.
 
     Parameters:
         df_labels (pd.DataFrame): DataFrame containing video labels.
         video_dir (str): Directory where the videos are stored.
-        output_dir (str): Directory where the images will be saved.
+        dataset_dir (str): Directory where the images will be saved.
 
     Returns:
         None
     """
     # Ensure the output directory exists
-    os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(dataset_dir, exist_ok=True)
 
     rescaled_videos_id = []
     for _, row in df_labels.iterrows():
@@ -40,13 +41,51 @@ def create_max_constriction_dataset(df_labels, video_dir, output_dir, frame_size
                 rescaled_videos_id.append(row.video_id)
 
             # Save the frame as an image
-            output_path = os.path.join(output_dir, f"{video_id}_max_constriction.png")
-            save_frame_as_image(frame, output_path)
+            output_path = os.path.join(dataset_dir, f"{video_id}_max_constriction.png")
+            save_image(frame, output_path)
 
         else:
             logging.error(f"Frame not found for video ID: {video_id} at frame number: {frame_number}")
 
     logging.info(f"{len(rescaled_videos_id)} videos needed to be resized to {frame_size[0]}x{frame_size[1]}: {rescaled_videos_id}")
 
+def create_all_frames_dataset(videos_dir, dataset_dir, frame_size=(512, 512)):
+    """
+    Create a dataset of all frames from videos.
+
+    Parameters:
+        videos_dir (str): Directory where the videos are stored.
+        dataset_dir (str): Directory where the images will be saved.
+
+    Returns:
+        None
+    """
+    os.makedirs(dataset_dir, exist_ok=True)
+    video_files = [f for f in os.listdir(videos_dir) if f.endswith('.avi')]
+
+    for video_file in video_files:
+        videos_id = os.path.splitext(video_file)[0]
+        video_path = os.path.join(videos_dir, video_file)
+        cap = cv.VideoCapture(video_path)
+        if not cap.isOpened():
+            logging.error(f"Error: Could not open video {video_path}.")
+            continue
         
+        frame_count = 0
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            
+            # Resize the frame if necessary
+            if (frame.shape[1], frame.shape[0]) != frame_size:
+                frame = cv.resize(frame, frame_size)
+
+            # Save the frame as an image
+            output_path = os.path.join(dataset_dir, f"{videos_id}_frame_{frame_count}.png")
+            save_image(frame, output_path)
+            frame_count += 1
         
+        cap.release()
+    logging.info(f"All frames from videos in {videos_dir} have been saved to {dataset_dir}.")
+
